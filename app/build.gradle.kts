c6c81f09cb726444aef39642cd4ecb606abebc49
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-08-21 00:02:29 +0800

    tmp: 触发更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.vincent.grainledger"
         minSdk = 33
         targetSdk = 37
-        versionCode = 120
-        versionName = "1.2.0"
+        versionCode = 121
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
